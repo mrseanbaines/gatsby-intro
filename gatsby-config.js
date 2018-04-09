b69bee415a5526be7b216c6993e10270d2949f5f
Sean Baines <mrseanbaines@gmail.com>
2018-04-10 00:01:11 +0100

    Add SASS plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Intro',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
 };
